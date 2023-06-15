a27b6f0ea258dcf97ceaf4c17cee98e27c2c0aef
Ваше Имя <you@example.com>
2023-06-16 03:08:05 +0400

    create bot

--- assets/constants.js
+++ assets/constants.js
@@ -5,6 +5,5 @@ export const API_URL = "http://api.weatherapi.com/v1";
 export const commands = [
   { command: "start", description: "Перезапустить бота" },
   { command: "weather", description: "Узнать погоду" },
-  { command: "t", description: "1" },
-  { command: "2", description: "f" },
+  { command: "1", description: "igor" },
 ];
